9d591a63e654ef5da8779b457445d19a51a66409
Kevin-Poli <khususmaen4154@gmail.com>
2024-09-03 13:15:07 +0700

    Akhirnya

---
+++ js2/bin/js2.dart
@@ -0,0 +1,7 @@
+import 'package:js2/js2.dart' as js2;
+
+void main() {
+  for (var i = 18; i >8; i--) {
+    print('Nama Saya Adalah Tukul , Sekarang berumur ${i +0}');
+  }
+}
---
+++ js2/test/js2_test.dart
@@ -0,0 +1,8 @@
+import 'package:js2/js2.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
