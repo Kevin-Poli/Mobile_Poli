b82c78087e16402ccf012346333372caf53dc47b
Kevin-Poli <khususmaen4154@gmail.com>
2024-11-19 11:09:04 +0700

    update last

--- JS_12/weather_app/lib/screens/homescreen.dart
+++ JS_12/weather_app/lib/screens/homescreen.dart
@@ -33,7 +33,7 @@ class _HomeScreenState extends State<HomeScreen> {
       if (weatherData != null) {
         setState(() {
           cityName = weatherData['name'];
-          temperature = weatherData['main']['temp'] - 273.15; // Kelvin to Celsius
+          temperature = weatherData['main']['temp'] - 273.15;
           humidity = weatherData['main']['humidity'];
           windSpeed = weatherData['wind']['speed'];
           cloudCover = weatherData['clouds']['all'];
@@ -62,34 +62,203 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Weather App'),
-      ),
       body: Container(
-        decoration: const BoxDecoration(
+        decoration: BoxDecoration(
           gradient: LinearGradient(
-            colors: [Color(0xffFA8BFF), Color(0xff2BD2FF), Color(0xff2BFF88)],
-            begin: Alignment.bottomLeft,
-            end: Alignment.topRight,
+            colors: [
+              Color(0xFF1E88E5),
+              Color(0xFF64B5F6),
+            ],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
           ),
         ),
-        padding: const EdgeInsets.all(16.0),
+        child: SafeArea(
+          child: Column(
+            children: [
+              _buildHeader(),
+              Expanded(
+                child: _buildWeatherContent(),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildHeader() {
+    return Container(
+      padding: EdgeInsets.all(16),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'Weather App',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+              IconButton(
+                icon: Icon(Icons.refresh, color: Colors.white),
+                onPressed: _fetchWeatherByLocation,
+              ),
+            ],
+          ),
+          SizedBox(height: 16),
+          SearchField(onSubmitted: _fetchWeatherByCity),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildWeatherContent() {
+    if (!isLoaded) {
+      return Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            SearchField(onSubmitted: _fetchWeatherByCity),
-            const SizedBox(height: 20),
-            if (isLoaded) ...[
-              WeatherCard(title: 'City', value: cityName),
-              WeatherCard(title: 'Temperature', value: '${temperature.toStringAsFixed(2)} °C'),
-              WeatherCard(title: 'Humidity', value: '$humidity %'),
-              WeatherCard(title: 'Wind Speed', value: '$windSpeed m/s'),
-              WeatherCard(title: 'Cloud Cover', value: '$cloudCover %'),
-              WeatherCard(title: 'Pressure', value: '$pressure hPa'),
-            ] else
-              const CircularProgressIndicator(),
+            CircularProgressIndicator(color: Colors.white),
+            SizedBox(height: 16),
+            Text(
+              'Getting weather information...',
+              style: TextStyle(color: Colors.white),
+            ),
           ],
         ),
+      );
+    }
+
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white.withOpacity(0.9),
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(30),
+          topRight: Radius.circular(30),
+        ),
+      ),
+      child: SingleChildScrollView(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          children: [
+            _buildMainWeatherInfo(),
+            SizedBox(height: 24),
+            _buildDetailedWeatherInfo(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildMainWeatherInfo() {
+    return Column(
+      children: [
+        Text(
+          cityName,
+          style: TextStyle(
+            fontSize: 32,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        SizedBox(height: 8),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              temperature > 20 ? Icons.wb_sunny : Icons.cloud,
+              size: 50,
+              color: Colors.orange,
+            ),
+            SizedBox(width: 16),
+            Text(
+              '${temperature.toStringAsFixed(1)}°C',
+              style: TextStyle(
+                fontSize: 48,
+                fontWeight: FontWeight.w200,
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Widget _buildDetailedWeatherInfo() {
+    return GridView.count(
+      shrinkWrap: true,
+      physics: NeverScrollableScrollPhysics(),
+      crossAxisCount: 2,
+      mainAxisSpacing: 16,
+      crossAxisSpacing: 16,
+      childAspectRatio: 1.5,
+      children: [
+        _buildWeatherDetailCard(
+          'Humidity',
+          '$humidity%',
+          Icons.water_drop,
+          Colors.blue,
+        ),
+        _buildWeatherDetailCard(
+          'Wind Speed',
+          '$windSpeed m/s',
+          Icons.air,
+          Colors.green,
+        ),
+        _buildWeatherDetailCard(
+          'Cloud Cover',
+          '$cloudCover%',
+          Icons.cloud_queue,
+          Colors.grey,
+        ),
+        _buildWeatherDetailCard(
+          'Pressure',
+          '$pressure hPa',
+          Icons.speed,
+          Colors.purple,
+        ),
+      ],
+    );
+  }
+
+  Widget _buildWeatherDetailCard(
+    String title,
+    String value,
+    IconData icon,
+    Color color,
+  ) {
+    return Container(
+      padding: EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        color: color.withOpacity(0.1),
+        borderRadius: BorderRadius.circular(15),
+        border: Border.all(color: color.withOpacity(0.3)),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(icon, color: color),
+          SizedBox(height: 8),
+          Text(
+            title,
+            style: TextStyle(
+              fontSize: 14,
+              color: Colors.grey[600],
+            ),
+          ),
+          SizedBox(height: 4),
+          Text(
+            value,
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+              color: Colors.black87,
+            ),
+          ),
+        ],
       ),
     );
   }
